dbed07397163afa710e7bc9d5c778fab7aa30bec
lopez <agustin.lopez2991@gmail.com>
2023-10-02 19:44:14 -0300

    Co-authored-by: nachoti19 <nachoti19@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,14 @@
+const cerrar = document.querySelector("#cerrar-hamburguesa");
+const abrir = document.querySelector("#abrir-hamburguesa");
+const hamburguesa = document.querySelector("#hamburguesa");
+
+abrir.addEventListener("click", show);
+cerrar.addEventListener("click", hide);
+
+function show() {
+    hamburguesa.classList.remove("hide");
+}
+
+function hide() {
+    hamburguesa.classList.add("hide");
+}
